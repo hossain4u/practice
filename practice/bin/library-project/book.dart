39045f122c3c448c737edb7f4b449e8e93e52ccb
hossain4u <sujonmazi20@gmail.com>
2024-09-26 22:19:02 +0600

    add library project

---
+++ practice/bin/library-project/book.dart
@@ -0,0 +1,21 @@
+class Book{
+  String title='';
+  String author='';
+  int publicationYear=0;
+  bool Available= true;
+
+  Book({required this.title,required this.author,required this.publicationYear});
+
+  void borrow(){
+
+  }
+
+  void returnBook(){
+
+  }
+
+  void available(){
+
+  }
+
+}
---
+++ practice/bin/library-project/library.dart
@@ -0,0 +1,19 @@
+import 'book.dart';
+class Library{
+  List <Book> books=[];
+
+  void addBook(Book book){
+
+  books.add(books);
+  }
+  void removeBook(){
+
+
+  }
+  void searchBookByTitle(){
+
+
+  }
+
+
+}
